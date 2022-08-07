fa177f146892802ec981c119d440f6120d007dff
Dany <mineralnone@gmail.com>
2022-08-07 04:09:54 +0300

    added filtering and pagination to the template

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
 			<section>
 				<div class="flex">
 					<div class="max-w-xs">
-						<label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
+						<label for="wallet" class="block text-sm font-medium text-gray-700">Ticker</label>
 						<div class="mt-1 relative rounded-md shadow-md">
 							<input
 								v-model="ticker"
@@ -38,7 +38,7 @@
 								name="wallet"
 								id="wallet"
 								class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-								placeholder="Например DOGE"
+								placeholder="Added coin"
 							/>
 						</div>
 						<!-- <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
@@ -63,7 +63,9 @@
 								CHD
 							</span>
 						</div> -->
-						<div v-if="alreadyTicker" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+						<div v-if="alreadyTicker" class="text-sm text-red-600">
+							This ticker has already been added
+						</div>
 					</div>
 				</div>
 				<button
@@ -84,7 +86,26 @@
 							d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
 						></path>
 					</svg>
-					Добавить
+					Add
+				</button>
+				<hr class="w-full border-t border-gray-600 my-4" />
+				<p for="wallet" class="block text-sm font-medium text-gray-700">Filter</p>
+				<input
+					type="text"
+					name="wallet"
+					id="wallet"
+					class="mt-1 block w-100 pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+					placeholder="Filter condition"
+				/>
+				<button
+					class="mr-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+				>
+					Back
+				</button>
+				<button
+					class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+				>
+					Next
 				</button>
 			</section>
 
@@ -125,7 +146,7 @@
 									d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
 									clip-rule="evenodd"
 								></path></svg
-							>Удалить
+							>Delete
 						</button>
 					</div>
 				</dl>
